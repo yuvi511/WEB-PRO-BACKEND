77f3f1ade2f6a22db7a2adf5c8df8fda33ec43c7
littlegamer365 <yuvrajsaha51103@gmail.com>
2023-04-02 17:25:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get('/',(req,res)=>{
+    res.end();
+})
+
+
+app.listen("3000",()=>{
+    console.log("Listening At Port 3000");
+})
