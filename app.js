9603a8ed197440b2fa901ae4868694ddc2c6a46f
littlegamer365 <yuvrajsaha51103@gmail.com>
2023-04-06 11:12:10 +0530

    Latest Commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const async = require('async');
 const notifier = require('node-notifier');
 
 
+
+
 const app = express();
 const port = 3000;
 const url = 'mongodb://localhost:27017';
@@ -75,17 +77,26 @@ app.post('/addProduct', async (req, res) => {
   res.send('Product added successfully');
 });
 
-app.post('/calculateTotalPrice', async (req, res) => {
+/*app.post('/calculateTotalPrice', async (req, res) => {
   const products = req.body.products;
   let totalPrice = 0;
-
+  
   for (let i = 0; i < products.length; i++) {
+    console.log('Product name:', products[i].name);
     const product = await Product.findOne({ name: products[i].name });
-    totalPrice += product.price * products[i].quantity;
+    console.log('Product:', product);
+    if (product) {
+      totalPrice += product.price;
+    } else {
+      // If product is null, handle the error and inform the user
+      res.status(400).send('Product not found: ' + products[i].name);
+      return;
+    }
   }
 
   res.send('Total Price: ' + totalPrice);
-});
+});*/
+
 
 
 
@@ -112,21 +123,230 @@ app.get('/login', async (req, res) => {
 
 
 
+app.use(express.json());
+
+/*app.get('/cart', async (req, res) => {
+  const products = await Product.find();
+  const options = products.map(product => `<option value="${product.name}">${product.name}</option>`).join('');
+  const form = `
+    <form action="/calculateTotalPrice" method="post">
+      <label for="product">Product:</label>
+      <select id="product" name="product">
+        ${options}
+      </select>
+      <br>
+      <label for="quantity">Quantity:</label>
+      <input type="number" id="quantity" name="quantity" min="1" value="1">
+      <br>
+      <button type="submit">Calculate Total Price</button>
+    </form>
+  `;
+  res.send(form);
+});
+
+// Handle form submission
+app.post('/calculateTotalPrice', async (req, res) => {
+  const productName = req.body.product;
+  const quantity = req.body.quantity;
+  const product = await Product.findOne({ name: productName });
+  if (product) {
+    const totalPrice = product.price * quantity;
+    res.send(`Total Price: ${totalPrice}`);
+  } else {
+    res.status(400).send(`Product not found: ${productName}`);
+  }
+});*/
+
+
+app.get('/cart', async (req, res) => {
+  const products = await Product.find();
+  const options = products.map(product => `<option value="${product.name}">${product.name}</option>`).join('');
+  const form = `
+    <style>
+      body {
+        background-color: #f8f9fa;
+      }
+      .form-container {
+        max-width: 500px;
+        margin: auto;
+        padding: 30px;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+      }
+      label {
+        margin-bottom: 5px;
+        font-weight: bold;
+      }
+      .form-control {
+        margin-bottom: 20px;
+      }
+      .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+      }
+      .btn-primary:hover {
+        background-color: #0069d9;
+        border-color: #0062cc;
+      }
+    </style>
+    <div class="container mt-5">
+      <div class="form-container">
+        <form action="/calculateTotalPrice" method="post">
+          <div class="form-group">
+            <label for="product">Product:</label>
+            <select class="form-control" id="product" name="product">
+              ${options}
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="quantity">Quantity:</label>
+            <input class="form-control" type="number" id="quantity" name="quantity" min="1" value="1">
+          </div>
+          <button type="submit" class="btn btn-primary">Calculate Total Price</button>
+        </form>
+      </div>
+    </div>
+  `;
+  res.send(form);
+  })
+
+  /*app.get('/cart', async (req, res) => {
+    const products = await Product.find();
+    const options = products.map(product => `<option value="${product.name}">${product.name}</option>`).join('');
+    const form = `
+      <html>
+        <head>
+          <title>Calculate Total Price</title>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" integrity="sha512-rsaeJkS8ffTQj/1wT+ry+jDyW8HvF2Q1A+vPrXzpyUaO8rDdntU6drwL6fWeOzBdqv/u7AEm44pG5X5R5+ZcMw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        </head>
+        <body>
+          <div class="container mt-5">
+            <div class="card">
+              <div class="card-body">
+                <h1 class="card-title">Calculate Total Price</h1>
+                <form action="/calculateTotalPrice" method="post">
+                  <div class="form-group">
+                    <label for="product">Product:</label>
+                    <select class="form-control" id="product" name="product">
+                      ${options}
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="quantity">Quantity:</label>
+                    <input class="form-control" type="number" id="quantity" name="quantity" min="1" value="1">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Calculate Total Price</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha512-C6It5L6MhUFOeMn+7xCM6Q2/K5Fn5jrHzn5w/OUb+FRnoQODj+Yf8L/1zofB1NlJc58o+Pd/j0wHVRrJZM/NtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        </body>
+      </html>
+    `;
+    res.send(form);
+  });*/
+  
+
+
+  /*app.post('/calculateTotalPrice', async (req, res) => {
+    const productName = req.body.product;
+    const quantity = req.body.quantity;
+    const product = await Product.findOne({ name: productName });
+    if (product) {
+      const totalPrice = product.price * quantity;
+      res.send(`Total Price: ${totalPrice}`);
+    } else {
+      res.status(400).send(`Product not found: ${productName}`);
+    }
+  });*/
+
+
+
+  /*app.post('/calculateTotalPrice', async (req, res) => {
+    const productName = req.body.product;
+    const quantity = req.body.quantity;
+    const product = await Product.findOne({ name: productName });
+  
+    if (product) {
+      const totalPrice = product.price * quantity;
+  
+      // Styling for the success message
+      const successMessage = `
+        <div style="background-color: #dff0d8; border-color: #d6e9c6; color: #3c763d; padding: 15px; margin-bottom: 20px; border-radius: 4px;">
+          Total Price: ${totalPrice}
+        </div>
+      `;
+  
+      res.send(successMessage);
+    } else {
+      // Styling for the error message
+      const errorMessage = `
+        <div style="background-color: #f2dede; border-color: #ebccd1; color: #a94442; padding: 15px; margin-bottom: 20px; border-radius: 4px;">
+          Product not found: ${productName}
+        </div>
+      `;
+  
+      res.status(400).send(errorMessage);
+    }
+  });*/
+
+
+  app.post('/calculateTotalPrice', async (req, res) => {
+    const productName = req.body.product;
+    const quantity = req.body.quantity;
+    const product = await Product.findOne({ name: productName });
+    if (product) {
+      const totalPrice = product.price * quantity;
+      const response = `
+        <html>
+          <head>
+            <title>Calculate Total Price</title>
+            <style>
+              body {
+                background-color: #f8f9fa;
+              }
+              .container {
+                max-width: 500px;
+                margin: auto;
+                padding: 30px;
+                background-color: #fff;
+                border-radius: 10px;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                font-size: 24px;
+                font-weight: bold;
+                margin-bottom: 20px;
+              }
+              p {
+                font-size: 18px;
+                font-weight: bold;
+              }
+            </style>
+          </head>
+          <body>
+            <div class="container">
+              <h1>Calculate Total Price</h1>
+              <p>Total Price: ${totalPrice}</p>
+            </div>
+          </body>
+        </html>
+      `;
+      res.send(response);
+    } else {
+      res.status(400).send(`Product not found: ${productName}`);
+    }
+  });
+  
+  
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-  app.listen("3000",()=>{
+app.listen("3000",()=>{
     console.log("listening to port 3000");
   });
 
