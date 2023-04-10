8df22fe64888c6b4133d0d7247667d052569f99c
littlegamer365 <yuvrajsaha51103@gmail.com>
2023-04-10 10:17:04 +0530

    New Update

--- app.js
+++ app.js
@@ -38,7 +38,24 @@ app.get('/', (req, res) => {
   
 });
 
+
 app.post('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.findOne({ username: username, password: password });
+
+  if (!user) {
+    res.send('Invalid username or password');
+    return;
+  }
+  
+  app.use(express.static(path.join(__dirname,"public")))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+
+/*app.post('/login', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -57,7 +74,7 @@ app.post('/login', async (req, res) => {
   }
   app.use(express.static(path.join(__dirname,"public")))
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+})*/
 
 
 const productSchema = new mongoose.Schema({
@@ -76,31 +93,6 @@ app.post('/addProduct', async (req, res) => {
 
   res.send('Product added successfully');
 });
-
-/*app.post('/calculateTotalPrice', async (req, res) => {
-  const products = req.body.products;
-  let totalPrice = 0;
-  
-  for (let i = 0; i < products.length; i++) {
-    console.log('Product name:', products[i].name);
-    const product = await Product.findOne({ name: products[i].name });
-    console.log('Product:', product);
-    if (product) {
-      totalPrice += product.price;
-    } else {
-      // If product is null, handle the error and inform the user
-      res.status(400).send('Product not found: ' + products[i].name);
-      return;
-    }
-  }
-
-  res.send('Total Price: ' + totalPrice);
-});*/
-
-
-
-
-
 
 
 
@@ -125,37 +117,7 @@ app.get('/login', async (req, res) => {
 
 app.use(express.json());
 
-/*app.get('/cart', async (req, res) => {
-  const products = await Product.find();
-  const options = products.map(product => `<option value="${product.name}">${product.name}</option>`).join('');
-  const form = `
-    <form action="/calculateTotalPrice" method="post">
-      <label for="product">Product:</label>
-      <select id="product" name="product">
-        ${options}
-      </select>
-      <br>
-      <label for="quantity">Quantity:</label>
-      <input type="number" id="quantity" name="quantity" min="1" value="1">
-      <br>
-      <button type="submit">Calculate Total Price</button>
-    </form>
-  `;
-  res.send(form);
-});
 
-// Handle form submission
-app.post('/calculateTotalPrice', async (req, res) => {
-  const productName = req.body.product;
-  const quantity = req.body.quantity;
-  const product = await Product.findOne({ name: productName });
-  if (product) {
-    const totalPrice = product.price * quantity;
-    res.send(`Total Price: ${totalPrice}`);
-  } else {
-    res.status(400).send(`Product not found: ${productName}`);
-  }
-});*/
 
 
 app.get('/cart', async (req, res) => {
@@ -211,86 +173,11 @@ app.get('/cart', async (req, res) => {
   res.send(form);
   })
 
-  /*app.get('/cart', async (req, res) => {
-    const products = await Product.find();
-    const options = products.map(product => `<option value="${product.name}">${product.name}</option>`).join('');
-    const form = `
-      <html>
-        <head>
-          <title>Calculate Total Price</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" integrity="sha512-rsaeJkS8ffTQj/1wT+ry+jDyW8HvF2Q1A+vPrXzpyUaO8rDdntU6drwL6fWeOzBdqv/u7AEm44pG5X5R5+ZcMw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        </head>
-        <body>
-          <div class="container mt-5">
-            <div class="card">
-              <div class="card-body">
-                <h1 class="card-title">Calculate Total Price</h1>
-                <form action="/calculateTotalPrice" method="post">
-                  <div class="form-group">
-                    <label for="product">Product:</label>
-                    <select class="form-control" id="product" name="product">
-                      ${options}
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="quantity">Quantity:</label>
-                    <input class="form-control" type="number" id="quantity" name="quantity" min="1" value="1">
-                  </div>
-                  <button type="submit" class="btn btn-primary">Calculate Total Price</button>
-                </form>
-              </div>
-            </div>
-          </div>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha512-C6It5L6MhUFOeMn+7xCM6Q2/K5Fn5jrHzn5w/OUb+FRnoQODj+Yf8L/1zofB1NlJc58o+Pd/j0wHVRrJZM/NtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        </body>
-      </html>
-    `;
-    res.send(form);
-  });*/
+  
   
 
 
-  /*app.post('/calculateTotalPrice', async (req, res) => {
-    const productName = req.body.product;
-    const quantity = req.body.quantity;
-    const product = await Product.findOne({ name: productName });
-    if (product) {
-      const totalPrice = product.price * quantity;
-      res.send(`Total Price: ${totalPrice}`);
-    } else {
-      res.status(400).send(`Product not found: ${productName}`);
-    }
-  });*/
-
-
-
-  /*app.post('/calculateTotalPrice', async (req, res) => {
-    const productName = req.body.product;
-    const quantity = req.body.quantity;
-    const product = await Product.findOne({ name: productName });
   
-    if (product) {
-      const totalPrice = product.price * quantity;
-  
-      // Styling for the success message
-      const successMessage = `
-        <div style="background-color: #dff0d8; border-color: #d6e9c6; color: #3c763d; padding: 15px; margin-bottom: 20px; border-radius: 4px;">
-          Total Price: ${totalPrice}
-        </div>
-      `;
-  
-      res.send(successMessage);
-    } else {
-      // Styling for the error message
-      const errorMessage = `
-        <div style="background-color: #f2dede; border-color: #ebccd1; color: #a94442; padding: 15px; margin-bottom: 20px; border-radius: 4px;">
-          Product not found: ${productName}
-        </div>
-      `;
-  
-      res.status(400).send(errorMessage);
-    }
-  });*/
 
 
   app.post('/calculateTotalPrice', async (req, res) => {
@@ -333,7 +220,7 @@ app.get('/cart', async (req, res) => {
               <h1>Calculate Total Price</h1>
               <p>Total Price: ${totalPrice} Rs</p>
             </div>
-          </body>
+          </body>/
         </html>
       `;
       res.send(response);
@@ -359,46 +246,25 @@ const userSchema2 = new mongoose.Schema({
   }
 });
 
-// Create the user model
+
 const UserN = mongoose.model('User2', userSchema2);
 
-// Define the route for handling form submissions
-/*app.get('/cart-success', (req, res) => {
-  // Get the user input data from the request parameters
-  const name = req.query.fname;
-  const email = req.query.email;
-  const phone = req.query.phone;
-
-  // Create a new user object with the input data
-  const newUser = new UserN({
-    name,
-    email,
-    phone
-  });
-
-  // Save the new user object to the database
-  newUser.save()
-    .then(() => {
-      res.send('Thank you for contacting us!');
-    })
-    .catch(err => console.log(err));
-});*/
 
 
 app.get('/cart-success', (req, res) => {
-  // Get the user input data from the request parameters
+  
   const name = req.query.fname;
   const email = req.query.email;
   const phone = req.query.phone;
 
-  // Create a new user object with the input data
+  
   const newUser = new UserN({
     name,
     email,
     phone
   });
 
-  // Save the new user object to the database
+  
   newUser.save()
     .then(() => {
       res.send('<div style="background-color: #C0DBEA; color: #721c24; padding: 10px; border-radius: 5px; margin-top: 10px;"><p style="font-size: 18px; font-weight: bold;">Thank you for contacting us!</p><p>We will get back to you shortly.</p></div>');
@@ -417,154 +283,4 @@ app.get('/cart-success', (req, res) => {
 app.listen("3000",()=>{
     console.log("listening to port 3000");
   });
-
-/*const bodyParser = require('body-parser');
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/products', { useNewUrlParser: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-// Create a product schema
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-});
-
-// Create a product model
-const Product = mongoose.model('Product', productSchema);
-
-// Create an express app
-
-
-// Use body-parser middleware to parse request bodies
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve the HTML page with the product dropdown
-app.get('/', async (req, res) => {
-  try {
-    // Get all products from the database
-    const products = await Product.find({}, 'name price');
-
-    // Render the HTML page with the product dropdown
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Add to Cart</title>
-        <style>
-          form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-top: 50px;
-            font-family: Arial, sans-serif;
-            font-size: 16px;
-          }
-          label {
-            margin-top: 10px;
-            margin-bottom: 5px;
-            text-align: center;
-            font-weight: bold;
-          }
-          input[type="submit"] {
-            margin-top: 20px;
-            padding: 10px;
-            background-color: #3498db;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 16px;
-            font-weight: bold;
-          }
-          input[type="submit"]:hover {
-            background-color: #2980b9;
-          }
-          #bill {
-            margin-top: 20px;
-            text-align: center;
-            font-weight: bold;
-          }
-        </style>
-      </head>
-      <body>
-        <form action="/" method="POST">
-          <label for="product">Product:</label>
-          <select id="product" name="product">
-            <option value="">Select a product...</option>
-            ${products.map(product => `
-              <option value="${product.name}|${product.price.toFixed(2)}">$${product.price.toFixed(2)} - ${product.name}</option>
-            `).join('')}
-          </select>
-          <label for="quantity">Quantity:</label>
-          <input type="number" id="quantity" name="quantity" min="1" max="10" value="1">
-          <input type="submit" value="Add to Cart">
-        </form>
-        <div id="bill">
-        </div>
-      </body>
-      </html>
-    `);
-  } catch (err) {
-    console.log(err);
-    res.send('Error retrieving products from database');
-  }
-});
-
-// Handle the form submission
-app.post('/', async (req, res) => {
-  try {
-    // Parse the selected product and quantity from the request body
-    // Find the selected product in the database
-const product = await Product.findOne({ name: productName });
-
-// If the product doesn't exist, throw an error
-if (!product) {
-  throw new Error(`Product "${productName}" not found`);
-}
-
-// Calculate the total price
-const totalPrice = product.price * quantity;
-
-// Render the HTML page with the bill
-res.send(`
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Add to Cart</title>
-    <style>
-      #bill {
-        margin-top: 50px;
-        text-align: center;
-        font-family: Arial, sans-serif;
-        font-size: 16px;
-        font-weight: bold;
-      }
-      #bill span {
-        color: #3498db;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="bill">
-      <span>Product:</span> ${productName}<br>
-      <span>Price:</span> $${productPrice}<br>
-      <span>Quantity:</span> ${quantity}<br>
-      <span>Total Price:</span> $${totalPrice.toFixed(2)}
-    </div>
-  </body>
-  </html>
-`);
-
-} catch (err) {
-  console.log(err);
-  res.send('Error adding product to cart: ${err.message}');
-  }
-  });
-*/
-
-
-
-
 
